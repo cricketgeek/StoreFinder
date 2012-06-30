a9116af65bb06f78a6cec1aa3adf32b60d3a9f36
cricketgeek <jonesmc123@gmail.com>
2012-06-30 11:33:56 -0500

    Initial Commit

---
+++ StoreFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreFinder
+//
+//  Created by Mark Jones on 6/30/12.
+//  Copyright (c) 2012 Geordie Enterprises LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ StoreFinder/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  StoreFinder
+//
+//  Created by Mark Jones on 6/30/12.
+//  Copyright (c) 2012 Geordie Enterprises LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
